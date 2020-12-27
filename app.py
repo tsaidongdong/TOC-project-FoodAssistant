b5fd30d057e7bd1e8ed7aaba43b26e418a8a2810
tsaidongdong <62095802+tsaidongdong@users.noreply.github.com>
2020-12-27 23:35:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -131,10 +131,10 @@ def webhook_handler():
     return "OK"
 
 
-@app.route("/show-fsm", methods=["GET"])
+""""@app.route("/show-fsm", methods=["GET"])
 def show_fsm():
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    return send_file("fsm.png", mimetype="image/png")"""
 
 
 if __name__ == "__main__":
